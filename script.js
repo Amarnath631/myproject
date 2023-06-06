1c15ba4707ddcbe40ab43a9712fe248ce3d2042c
Amarnath631 <135607947+Amarnath631@users.noreply.github.com>
2023-06-06 10:45:00 +0530

    Update script.js
    changed variable name

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function fun() {  
-    var x = document.getElementById("frm1").value;  
-     alert("Hi"+" "+x+ "you have successfully submitted the details");  
+    var username = document.getElementById("frm1").value;  
+     alert("Hi"+" "+username+ "you have successfully submitted the details");  
  }  
